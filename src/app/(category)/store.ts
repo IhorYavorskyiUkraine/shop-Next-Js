66d7b949db14e0aa6b8ece4d5313587421517293
IhorYavorskyiUkraine <yapazhiloyigogosha@gmail.com>
2024-11-06 10:12:20 +0200

    test2
    test2

--- src/app/(category)/store.ts
+++ src/app/(category)/store.ts
@@ -14,6 +14,7 @@ export const useCategoryStore = create<CartStore>(set => ({
    filteredProducts: [],
    loading: true,
    error: false,
+   //test
    setFilteredProducts: filteredProducts =>
       set({ filteredProducts, loading: false, error: false }),
 }));
